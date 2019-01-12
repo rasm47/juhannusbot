3111e25ff42a00413a391ff42edce0d77b6fc537
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-01-12 10:34:10 +0200

    Add horoscope command

--- jbot.go
+++ jbot.go
@@ -6,7 +6,24 @@ import (
     "strings"
     "os"
     "bufio"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+    "bytes"
 )
+
+type horoscopeMeta struct {
+    Intensity string `json:"intensity"`
+    Keywords  string `json:"keywords"`
+    Mood      string `json:"mood"`
+}
+
+type horoscopeResponse struct {
+    Date      string `json:"date"`
+    Sunsign   string `json:"sunsign"`
+    Horoscope string `json:"horoscope"`
+    Meta      horoscopeMeta `json:"meta"`
+}
 
 func main() {
 
@@ -43,6 +60,44 @@ func main() {
             tosend := "world!"
             msg := tgbotapi.NewMessage(update.Message.Chat.ID, tosend)
             bot.Send(msg)
+            
+        } else if strings.HasPrefix(gotmsg, "/horos"){
+            horoscopeSign := ParseHoroscopeMessage(gotmsg)
+            if horoscopeSign == "" {
+                continue
+            }
+
+            response, err := http.Get("http://theastrologer-api.herokuapp.com/api/horoscope/" + horoscopeSign + "/today")
+            if err != nil {
+                log.Fatal(err)
+            } else {
+                defer response.Body.Close()
+                bodyBytes, err := ioutil.ReadAll(response.Body)
+                if err != nil {
+                    log.Fatal(err)
+                } else {
+                    bodyStr := string(bodyBytes)
+                    log.Printf(bodyStr)
+
+                    //reset the response body to the original unread state
+                    response.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+                    var hresponse horoscopeResponse
+                    err := json.Unmarshal(bodyBytes, &hresponse)
+                    if err != nil {
+                        log.Panic(err)
+                    } else {
+                        tosend := "Enkelit välittävät horoskooppinne:\n" +
+                        hresponse.Horoscope + "\n\nAvainsanat: " +
+                        hresponse.Meta.Keywords + "\nTunnetila: " +
+                        hresponse.Meta.Mood  + "\n\nHoroskooppi väittyi energiatasolla " +
+                        hresponse.Meta.Intensity + "."
+                        msg := tgbotapi.NewMessage(update.Message.Chat.ID, tosend)
+                        bot.Send(msg)
+                    }
+                }
+            }
+
         }
     }
 }
@@ -63,6 +118,37 @@ func FileToLines(filePath string) (lines []string, err error) {
       }
       err = scanner.Err()
       return
+}
+
+func ParseHoroscopeMessage(originalMessage string) string {
+    msg := strings.ToLower(originalMessage)
+    if strings.Contains(msg, "oina"){
+        return "aries"
+    } else if strings.Contains(msg, "härk"){
+        return "taurus"
+    } else if strings.Contains(msg, "kaks"){
+        return "gemini"
+    } else if strings.Contains(msg, "rap"){
+        return "cancer"
+    } else if strings.Contains(msg, "leij"){
+        return "leo"
+    } else if strings.Contains(msg, "neit"){
+        return "virgo"
+    } else if strings.Contains(msg, "vaa"){
+        return "libra"
+    } else if strings.Contains(msg, "skor"){
+        return "scorpio"
+    } else if strings.Contains(msg, "jous"){
+        return "sagittarius"
+    } else if strings.Contains(msg, "vesi"){
+        return "capricorn"
+    } else if strings.Contains(msg, "kaur"){
+        return "aquarius"
+    } else if strings.Contains(msg, "kal"){
+        return "pisces"
+    } else {
+        return ""
+    }
 }
 
 
