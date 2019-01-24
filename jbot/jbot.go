867197938686c697ff13f82e1d8cdb1b62b22788
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-01-24 21:07:59 +0200

    Put horoscope signs to an enum

--- jbot/jbot.go
+++ jbot/jbot.go
@@ -59,53 +59,105 @@ func HandleUpdate(bot *tgbotapi.BotAPI, book []string, update tgbotapi.Update) (
     return
 }
 
-type horoscopeMeta struct {
+// horoscopeResponseData contains data from
+// a certain REST API json response 
+type horoscopeResponseData struct {
+    Date      string `json:"date"`
+    Sunsign   string `json:"sunsign"`
+    Horoscope string `json:"horoscope"`
+    Meta      horoscopeResponseMetaData `json:"meta"`
+}
+
+// horoscopeResponseMetaData contains data from 
+// a certain REST API json response
+type horoscopeResponseMetaData struct {
     Intensity string `json:"intensity"`
     Keywords  string `json:"keywords"`
     Mood      string `json:"mood"`
 }
 
-type horoscopeResponse struct {
-    Date      string `json:"date"`
-    Sunsign   string `json:"sunsign"`
-    Horoscope string `json:"horoscope"`
-    Meta      horoscopeMeta `json:"meta"`
+
+// golang has no native support for enums,
+// so each horoscope is associated with a number.
+type horoscopeSign int 
+const (
+    horoscopeSignNone        horoscopeSign = 0
+    horoscopeSignAries       horoscopeSign = 1
+    horoscopeSignTaurus      horoscopeSign = 2
+    horoscopeSignGemini      horoscopeSign = 3
+    horoscopeSignCancer      horoscopeSign = 4
+    horoscopeSignLeo         horoscopeSign = 5
+    horoscopeSignVirgo       horoscopeSign = 6
+    horoscopeSignLibra       horoscopeSign = 7
+    horoscopeSignScorpio     horoscopeSign = 8
+    horoscopeSignSagittarius horoscopeSign = 9
+    horoscopeSignCapricorn   horoscopeSign = 10
+    horoscopeSignAquarius    horoscopeSign = 11
+    horoscopeSignPisces      horoscopeSign = 12
+)
+
+// String method for type horoscopeSign
+func (sign horoscopeSign) String() string {
+    //set out of range to horoscopeSignNone
+    if sign < 0 || sign > 12 {
+        return ""
+    }
+    
+    signs := [13]string{
+        "",
+        "Aries",
+        "Taurus",
+        "Gemini",
+        "Cancer",
+        "Leo",
+        "Virigo",
+        "Libra",
+        "Scorpio",
+        "Sagittrius",
+        "Capricorn",
+        "Aquarius",
+        "Pisces",
+        }
+        
+        return signs[sign]
 }
 
-func parseHoroscopeMessage(originalMessage string) string {
+// parseHoroscopeMessage searches originalMessage for certain
+// key phrases and returns a horoscopeSign if one is found.
+func parseHoroscopeMessage(originalMessage string) horoscopeSign {
     msg := strings.ToLower(originalMessage)
     if strings.Contains(msg, "oina"){
-        return "aries"
+        return horoscopeSignAries
     } else if strings.Contains(msg, "härk"){
-        return "taurus"
+        return horoscopeSignTaurus
     } else if strings.Contains(msg, "kaks"){
-        return "gemini"
+        return horoscopeSignGemini
     } else if strings.Contains(msg, "rap"){
-        return "cancer"
+        return horoscopeSignCancer
     } else if strings.Contains(msg, "leij"){
-        return "leo"
+        return horoscopeSignLeo
     } else if strings.Contains(msg, "neit"){
-        return "virgo"
+        return horoscopeSignVirgo
     } else if strings.Contains(msg, "vaa"){
-        return "libra"
+        return horoscopeSignLibra
     } else if strings.Contains(msg, "skor"){
-        return "scorpio"
+        return horoscopeSignScorpio
     } else if strings.Contains(msg, "jous"){
-        return "sagittarius"
+        return horoscopeSignSagittarius
     } else if strings.Contains(msg, "vesi"){
-        return "capricorn"
+        return horoscopeSignCapricorn
     } else if strings.Contains(msg, "kaur"){
-        return "aquarius"
+        return horoscopeSignAquarius
     } else if strings.Contains(msg, "kal"){
-        return "pisces"
+        return horoscopeSignPisces
     } else {
-        return ""
+        return horoscopeSignNone
     }
 }
 
-func resolveHoroscope(sign string) (reply string, err error) {
+func resolveHoroscope(sign horoscopeSign) (reply string, err error) {
     
-    response, err := http.Get("http://theastrologer-api.herokuapp.com/api/horoscope/" + sign + "/today")
+    response, err := http.Get("http://theastrologer-api.herokuapp.com/api/horoscope/" + sign.String() + "/today")
     if err != nil {
         return
     }
@@ -116,7 +168,7 @@ func resolveHoroscope(sign string) (reply string, err error) {
         return
     }
     
-    var hresponse horoscopeResponse
+    var hresponse horoscopeResponseData
     err = json.Unmarshal(bodyBytes, &hresponse)
     if err != nil {
         return
@@ -149,12 +201,12 @@ func createResponse(message string, book []string) (response string, err error) 
         response = "world!"
         
     } else if strings.HasPrefix(message, "/horos"){
-        horoscopeSign := parseHoroscopeMessage(message)
-        if horoscopeSign == "" {
-            response = ""
+        sign := parseHoroscopeMessage(message)
+        if sign == horoscopeSignNone {
+            response = horoscopeSignNone.String()
             return
         }
-        response, err = resolveHoroscope(horoscopeSign)
+        response, err = resolveHoroscope(sign)
         if err != nil {
             return
         }
