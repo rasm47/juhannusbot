fca9de88dec8164576a1488700598c2b43370231
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-06-05 16:38:29 +0300

    Remove unused util functions

--- jbot/util.go
+++ jbot/util.go
@@ -2,34 +2,6 @@ package jbot
 
 import "strings"
 
-// stringSlicesAreEqual returns true if two string slices (a and b) are fully equal.
-func stringSlicesAreEqual(a []string, b []string) bool {
-	if (a == nil) != (b == nil) {
-		return false
-	}
-
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func stringContainsAnyPattern(s string, patterns []string) bool {
-	for _, pattern := range patterns {
-		if strings.Contains(s, pattern) {
-			return true
-		}
-	}
-	return false
-}
-
 func stringHasAnyPrefix(s string, prefixes []string) bool {
 	for _, prefix := range prefixes {
 		if strings.HasPrefix(s, prefix) {
