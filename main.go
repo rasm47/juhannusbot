d1663b863b8802eb6b3274fa32d7ae92de9075cd
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-06-05 15:42:25 +0300

    Print error in main.go instead of Panic

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-    "log"
-    
-    "github.com/ruoskija/juhannusbot/jbot"
+	"log"
+
+	"github.com/ruoskija/juhannusbot/jbot"
 )
 
 func main() {
 
-    err := jbot.Start()
-    if err != nil {
-        log.Panic(err)
-    }
-    
+	err := jbot.Start()
+	if err != nil {
+		log.Printf("Closing bot due to error: %v", err)
+	}
+
+	return
 }
