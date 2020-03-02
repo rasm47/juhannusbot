3bbb0b8506326e6471cb1f5a902bc2d7897f2189
rasm47 <16821766+rasm47@users.noreply.github.com>
2020-03-02 18:17:35 +0200

    Add more tests to decide.go

--- jbot/decide_test.go
+++ jbot/decide_test.go
@@ -18,7 +18,9 @@ func TestFilterWordsTypical(t *testing.T) {
 
 	filtered := filterWords(testWords, wordsToFilter)
 	if len(filtered) != 4 {
-		t.Fatalf("%v did not reduce its length to 4 when filtered with %v", testWords, wordsToFilter)
+		t.Fatalf("%v did not reduce its length to 4 when filtered with %v",
+			testWords,
+			wordsToFilter)
 	}
 	return
 }
@@ -36,7 +38,48 @@ func TestFilterWordsEmpty(t *testing.T) {
 
 	filtered := filterWords(testWords, wordsToFilter)
 	if len(filtered) != 5 {
-		t.Fatalf("%v changed with empty filtering", testWords)
+		t.Fatalf("length of %v changed with empty filtering", testWords)
+	}
+	return
+}
+
+func TestDuplicateWordsTypical(t *testing.T) {
+	testWords := []string{
+		"word1",
+		"word2",
+		"word3",
+		"word4",
+		"word5",
+	}
+
+	wordsToDuplicate := []string{
+		"word2",
+		"word99",
+	}
+
+	duplicated := duplicateWords(testWords, wordsToDuplicate)
+	if len(duplicated) != 6 {
+		t.Fatalf("%v did not increase its length to 6 when duplicated with %v",
+			testWords,
+			wordsToDuplicate)
+	}
+	return
+}
+
+func TestDuplicateWordsEmpty(t *testing.T) {
+	testWords := []string{
+		"word1",
+		"word2",
+		"word3",
+		"word4",
+		"word5",
+	}
+
+	wordsToDuplicate := []string{}
+
+	duplicated := duplicateWords(testWords, wordsToDuplicate)
+	if len(duplicated) != 5 {
+		t.Fatalf("length of %v changed with no duplicating", testWords)
 	}
 	return
 }
